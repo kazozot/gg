8f25e4741c60f9e329a3408e9fb25a67ece7b6fc
kazozot <92520902+kazozot@users.noreply.github.com>
2025-11-15 19:54:07 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,6 +1,16 @@
 // --- KONFIGURASI WEBSITE ---
 
 /**
+ * [KODE BARU] Kontrol Global untuk Fitur Iklan
+ * * Ubah nilai di bawah ini untuk mengaktifkan atau menonaktifkan
+ * semua iklan di seluruh website.
+ * * - true:  Menampilkan iklan.
+ * * - false: Menyembunyikan semua iklan.
+ */
+const ENABLE_ADS = true;
+
+
+/**
  * Kontrol Global untuk Fitur Komentar (Giscus)
  * * Ubah nilai di bawah ini untuk mengaktifkan atau menonaktifkan
  * fitur komentar (diskusi) di seluruh website.
@@ -8,3 +18,46 @@
  * - false: Menyembunyikan seluruh bagian kolom komentar.
  */
 const ENABLE_COMMENTS = true;
+
+
+// --- [MODIFIKASI BARU] KONFIGURASI TAFSIR ---
+
+/**
+ * Definisikan sumber data tafsir yang tersedia.
+ * Kunci (misal 'kemenag') harus unik dan akan disimpan di localStorage.
+ * 'name' adalah teks yang akan tampil di dropdown.
+ * 'data' adalah nama variabel global yang berisi data tafsir dari file .js-nya.
+ */
+const TAFSIR_SOURCES = {
+    'kemenag': {
+        name: "Tafsir Kemenag",
+        data: TAFSIR_KEMENAG // Variabel dari js/data/tafsir-kemenag.js
+    },
+    'jalalayn': {
+        name: "Tafsir Jalalayn",
+        data: TAFSIR_JALALAYN // Variabel dari js/data/tafsir-jalalayn.js
+    },
+    'muntakhab': {
+        // [MODIFIKASI BARU] Nama diubah
+        name: "Tafsir Quraish Shihab", 
+        // [AKHIR MODIFIKASI]
+        data: TAFSIR_MUNTAKHAB // Variabel dari js/data/tafsir-muntakhab.js
+    },
+    'sabiq': {
+        name: "Tafsir As-Sabiq",
+        data: TAFSIR_SABIQ // Variabel dari js/data/tafsir-sabiq.js
+    },
+    'kingfahd': {
+        name: "Tafsir King Fahd",
+        data: TAFSIR_KINGFAHD // Variabel dari js/data/tafsir-kingfahd.js
+    }
+    // Anda bisa tambahkan sumber lain di sini di masa depan
+};
+
+/**
+ * Tentukan tafsir default yang akan dimuat saat pengguna pertama kali membuka.
+ * Gunakan kunci yang sama dari TAFSIR_SOURCES.
+ */
+const DEFAULT_TAFSIR = 'kemenag';
+
+// --- [AKHIR MODIFIKASI] ---
