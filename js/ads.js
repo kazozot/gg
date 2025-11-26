223f795f65b12781534612be263eabd193d9e022
kazozot <92520902+kazozot@users.noreply.github.com>
2025-11-26 21:29:03 +0700

    Create ads.js

---
+++ js/ads.js
@@ -0,0 +1,4 @@
+// File ini berfungsi sebagai "umpan".
+// AdBlock biasanya akan memblokir file yang bernama "ads.js".
+// Jika variabel ini terbaca true, berarti tidak ada AdBlock.
+var canRunAds = true;
