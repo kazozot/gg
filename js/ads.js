5f77f332e72852b43e8100c9cb1800084b87591a
kazozot <92520902+kazozot@users.noreply.github.com>
2025-11-26 22:06:02 +0700

    Update ads.js

--- js/ads.js
+++ js/ads.js
@@ -1,4 +1,3 @@
-// File: gg-main/js/ads.js
-// Kita set variabel global. Jika file ini diblokir, variabel ini tidak akan ada.
-window.adBlockerDisabled = true;
-console.log("Ads.js berhasil dimuat"); // Untuk debugging di console
+// File Umpan
+var adblock_test_variable = true;
+console.log("Ads.js loaded successfully");
